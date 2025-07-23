6be0e10e6e2e36d894cf4d6d795304f191a0c7db
Daniel <dphung@uwaterloo.ca>
2025-07-23 13:23:07 -0400

    Fix build break

--- app/src/main/java/com/example/recipttracker/data/local/UserDao.kt
+++ app/src/main/java/com/example/recipttracker/data/local/UserDao.kt
@@ -3,6 +3,7 @@ package com.example.recipttracker.data.local
 import androidx.room.*
 import com.example.recipttracker.domain.model.User
 import kotlinx.coroutines.flow.Flow
+import java.util.UUID
 
 @Dao
 interface UserDao {
